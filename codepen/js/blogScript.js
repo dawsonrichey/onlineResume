b17f6455764c8ad6e90cc192b85ffacb44e28282
dawsonrichey <dawsonrichey89@gmail.com>
2018-03-05 22:48:07 -0500

    CodePen Blog project added

---
+++ codepen/js/blogScript.js
@@ -0,0 +1,6 @@
+// https://github.com/josephschmitt/Clamp.js
+var module = document.getElementById("clampjs");
+$clamp(module, {clamp: 3});
+
+// var module = document.getElementById("getit");
+// $clamp(module, {clamp: 3});
